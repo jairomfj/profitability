2eb406ba42d166877b87335941ee016a231f0cc8
Agnaldo de Oliveira <agnaldo4j@gmail.com>
2016-01-03 16:12:30 -0200

    first plug example

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Profitability.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :mariaex, :ecto, :cowboy, :plug]]
   end
 
   # Dependencies can be Hex packages:
