e323d60f2da967bc5357078d8a75f081fb78fa86
Agnaldo de Oliveira <agnaldo4j@gmail.com>
2016-01-03 13:33:32 -0200

    initial commit

---
+++ lib/profitability.ex
@@ -0,0 +1,2 @@
+defmodule Profitability do
+end
---
+++ test/profitability_test.exs
@@ -0,0 +1,8 @@
+defmodule ProfitabilityTest do
+  use ExUnit.Case
+  doctest Profitability
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
